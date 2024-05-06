a43151ad9d63b0fd0d5ae2527d7594bd01fa1f79
David <david@bluepajamas.org>
2024-05-07 00:56:49 +0300

    updates

--- app.py
+++ app.py
@@ -1,60 +1,39 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import os
-import sqlite
+
+
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URL'] = 'SQLITE:///login.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Frequency.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Sapele10035@localhost/Frequency_test'
 db = SQLAlchemy(app)
 
-class Friends(db.model):
-    id = db.column(db.integer, primary_key=True)
-    name = db.column(db.string(200), nullable=False)
-    email = db.column(db.string(200), nullable=False)
-    date_created = db.column(db.Datetime, default=datetime.utcnow)       
-
-    def __repr__(self):
-        return '<name %r>' % self.id
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-
-@app.route("/", methods = ["POST"])
-def index():
-    UN = request.form['name']
-    PW = request.form['password']
-
-    sqlconnection = sqlite3.connection(currentlocation + "\login.db")
-    cursor = sqlconnection.cursor()
-    query1 = "SELECT Username, Password from users WHERE Username = '{un}' AND Password = '{pw})'".format(un = UN, pw = PW)
-
-    rows = cursor.execute(query1)
-    rows = rows.fetchall()
-    if len(rows) ==1:
-        return render_template("index.html")
-    else:
-        return redirect("/Getstarted")
+class Frequency(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.Integer, nullable=False)     
     
-@app.route("/Getstarted", methods= ["POST", "GET"])
-def Getstarted():
-    if request.method == "POST":
-        dUN = request.form['Username']
-        dPW = request.form['Password']
-        Uemail = request.form['emailuser']
-        sqlconnection = sqlite3.connection(currentlocation + "\login.db")
-        cursor = sqlconnection.cursor()
-        query1 = "INSERT INTO users VALUES('{u}','{p}','{e}')".format (u = dUN, p = dPW, e = Uemail)
-        cursor.execute(query1)
-        sqlconnection.commit()
-        return redirect("/")
-    return render_template("index.html")
+    def __repr__(self):
+        return '<Name %r>'%self.id
 
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if (request.method == "POST"):
+        sign_in = request.form['name']
+        in_sign = Frequency(name=sign_in ,password=request.form['password'], email=request.form['Email'])
 
-
+        try:
+            db.session.add(in_sign)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except:
+            return "An error occurred"
+    
+    else:
+        return render_template('index.html')
 
 
 @app.route('/jobs/')
